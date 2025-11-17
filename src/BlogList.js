535256f3b91ab2ea6380aab857518a6c7435bea6
IvanIvPe <pesicivan2002@gmail.com>
2025-11-17 16:13:21 +0100

    Blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,13 +1,14 @@
-const BlogList = (props) => {
+const BlogList = ({blogs,title}) => {
 
-    const blogs = props.blogs;
+    //const blogs = props.blogs;
+    //const title = props.title;
 
-    console.log(props, blogs);
 
 
     return ( 
         <div className="blog-list">
-                        {blogs.map((blog) => (
+            <h2>{ title }</h2>
+            {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author }</p>
@@ -15,6 +16,6 @@ const BlogList = (props) => {
             ))}
         </div>
      );
-}
+} 
  
 export default BlogList;
