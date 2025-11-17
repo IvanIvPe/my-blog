da38fe65e5a155649b7b95df255162e90fe6dc7e
IvanIvPe <pesicivan2002@gmail.com>
2025-11-17 15:38:31 +0100

    Blogs

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 const Home = () => {
-    return ( 
+    const [blogs, setBlogPosts] = useState([
+        { id: 1, title: "First Post", content: "This is the content of the first post.", author: "Alice", id: 1 },
+        { id: 2, title: "Second Post", content: "This is the content of the second post.", author: "Bob", id: 2 },
+        { id: 3, title: "Third Post", content: "This is the content of the third post.", author: "Charlie", id: 3 },
+    ]);
+
+    return (
         <div className="home">
-            <h2>Home Page</h2>
-            <p>Welcome to the home page of the blog!</p>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author }</p>
+
+                </div>
+            ))}
         </div>
-     );
+    );
 }
- 
+
 export default Home;
