d69852a7f6bc3a1fce3778826f57319e94c68277
IvanIvPe <pesicivan2002@gmail.com>
2025-11-17 16:28:47 +0100

    Blogs

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,17 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { id: 1, title: "First Post", content: "This is the content of the first post.", author: "Alice"},
         { id: 2, title: "Second Post", content: "This is the content of the second post.", author: "Bob"},
-        { id: 3, title: "Third Post", content: "This is the content of the third post.", author: "Charlie"}
+        { id: 3, title: "Third Post", content: "This is the content of the third post.", author: "Bob"}
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    };
 
     return (
         <div className="home">
-            <BlogList blogs= {blogs} title="All Blogs:" />
+            <BlogList blogs= {blogs} title="All Blogs:" handleDelete={handleDelete}/>
         </div>
     );
 }
