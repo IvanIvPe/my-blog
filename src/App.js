dee9f7c2c20ac7ec86fbdb896618f350c9b2a930
IvanIvPe <pesicivan2002@gmail.com>
2025-11-17 14:11:54 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="content">
+        <h1>Welcome to My Blog</h1>
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
