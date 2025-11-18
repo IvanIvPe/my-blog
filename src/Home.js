563edd9e4d77784a87c76b5f8e1a87a437ebb265
IvanIvPe <pesicivan2002@gmail.com>
2025-11-18 13:28:31 +0100

    Db JSON

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { id: 1, title: "First Post", content: "This is the content of the first post.", author: "Alice"},
-        { id: 2, title: "Second Post", content: "This is the content of the second post.", author: "Bob"},
-        { id: 3, title: "Third Post", content: "This is the content of the third post.", author: "Bob"}
-    ]);
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    };
-
-    return (
-        <div className="home">
-            <BlogList blogs= {blogs} title="All Blogs:" handleDelete={handleDelete}/>
-        </div>
-    );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
-
+ 
 export default Home;
